864fc31f7cbfd1d8ab002cb48512e6b10f898e7e
Iqbal Sahrizal <54559728+iqbal2906@users.noreply.github.com>
2022-03-22 20:32:10 +0700

    last commit

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
-import { FaFacebook, FaWhatsapp } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <BsLinkedin />
+        <a
+          href="https://www.linkedin.com/in/iqbal-sahrizal-994299135/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
       </div>
       <div>
-        <BsTwitter />
+        <a
+          href="https://twitter.com/Iqbal_S29/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
       </div>
       <div>
-        <BsInstagram />
-      </div>
-      <div>
-        <FaWhatsapp />
+        <a
+          href="https://www.instagram.com/iqbaaltanpa.e/?hl=id"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsInstagram />
+        </a>
       </div>
     </div>
   );
